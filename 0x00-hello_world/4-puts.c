0e3e38d35045bf822bc3d32b23f5708421dac219
Fredy Acuna <fredhiixd@gmail.com>
2020-02-05 17:06:20 +0000

    Added C mandatory tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string
+ * Description: Prints a string with space at the end
+ * Return: A string an a 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+};

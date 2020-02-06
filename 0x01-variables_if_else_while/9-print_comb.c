bcb9b51e6c93fb3f3d47c66c0595217c5b76d25f
Fredy Acuna <fredhiixd@gmail.com>
2020-02-06 17:47:55 +0000

    Added C Variables mandatory tasks

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers
+ * Description: For loop that prints the numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(', ');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

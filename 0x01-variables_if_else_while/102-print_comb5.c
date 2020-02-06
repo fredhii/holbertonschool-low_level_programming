65a19e5b895957a7207bfcade4a82b3b84114bc9
Fredy Acuna <fredhiixd@gmail.com>
2020-02-06 21:18:28 +0000

    Corrected C variables advanced tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,37 +6,31 @@
  */
 int main(void)
 {
-	int a;
-	int b;
-	int c;
-	int d;
+int a;
+int b;
+int c;
+int d;
 
-	for (a = '0'; a <= '9'; a++)
-	{
-		for (b = '0'; b <= '9'; b++)
-		{
-			for (c = '0'; c <= '9'; c++)
+for (a = '0'; a <= '9'; a++)
+	for (b = '0'; b <= '9'; b++)
+		for (c = '0'; c <= '9'; c++)
+			for (d = '0'; d <= '9'; d++)
 			{
-				for (d = '0'; d <= '9'; d++)
+				if (c >= a + 1 || d >= b + 1)
 				{
-					if (c >= a + 1 || d >= b + 1)
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (!(a == '9' || b == '8' || c == '9'
+					      || d == '9'))
 					{
-						putchar(a);
-						putchar(b);
+						putchar(',');
 						putchar(' ');
-						putchar(c);
-						putchar(d);
-						if (a != '9' || b != '8' || c !=
-						    '9' || d != '9')
-						{
-							putchar(',');
-							putchar(' ');
-						}
 					}
 				}
 			}
-		}
-	}
 	putchar('\n');
 	return (0);
 }

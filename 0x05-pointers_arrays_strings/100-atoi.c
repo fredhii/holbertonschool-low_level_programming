38d7f6fdf6d844a02f30753f7af27fe8f7a634cf
Fredy Acuna <fredhiixd@gmail.com>
2020-02-18 12:05:16 +0000

    Corrected pointers advanced tasks

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,10 +6,9 @@
  */
 int _atoi(char *s)
 {
-	int i;
 	int n;
 	int sign = 1;
-	unsigned int res;
+        int res;
 
 	for (n = 0; *(s + n) != '\0'; n++)
 	{
@@ -17,9 +16,8 @@ int _atoi(char *s)
 			sign *= -1;
 		if (*(s + n) >= '0' && *(s + n) <= '9')
 		{
-			i = *(s + n) - '0';
-			res = (res * 10) + i;
-			if (!(*(s + (n + 1)) >= '0' && *(s + (n + 1)) <= '9'))
+			res = (*(s + n) - '0') + res * 10;
+			if (*(s + (n + 1)) <= '0' && *(s + (n + 1)) >= '9'))
 				break;
 		}
 	}

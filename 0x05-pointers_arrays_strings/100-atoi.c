bb025070306cfd5434a5bc63ebc718d8e5abe05a
Fredy Acuna <fredhiixd@gmail.com>
2020-02-19 20:36:27 +0000

    Change int to unsigned int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,23 @@
 #include "holberton.h"
 /**
- * _atoi - converts string to integer.
- * @s: string to convert.
- * Return: integer.
+ * _atoi - string to integer
+ * @s: string to transform
+ * Return: integer
  */
 int _atoi(char *s)
 {
+	int n;
 	int sign = 1;
 	unsigned int res = 0;
 
-	do {
-		if (*s == '-')
+	for (n = 0; *(s + n) != '\0'; n++)
+	{
+		if (*(s + n) == '-')
 			sign *= -1;
-
-		else if (*s >= '0' && *s <= '9')
-			res = (res * 10) + (*s - '0');
-
+		if (*(s + n) >= '0' && *(s + n) <= '9')
+			res = res * 10 + (*(s + n) - '0');
 		else if (res > 0)
 			break;
-	} while (*s++);
-
+	}
 	return (res * sign);
 }

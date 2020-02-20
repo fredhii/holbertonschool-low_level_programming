2925e2b23d44cf812dadee69a197b789d37361a2
Fredy Acuna <fredhiixd@gmail.com>
2020-02-20 17:49:42 +0000

    Added more pointers mandatory tasks

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * cap_string - Capitalize words
+ * @s: string to modify.
+ * Return: string.
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char mark[13] = {9, ' ', '\n', ',', ';', '.', '!', '?', '"', '(', ')',
+			 '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			for (j = 0; mark[j] != '\0'; j++)
+			{
+				if (s[i - 1] == mark[j])
+					s[i] -= 32;
+			}
+	}
+	return (s);
+}

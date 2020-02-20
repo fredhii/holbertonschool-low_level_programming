f15e407054b47fdf8f97fecdfac6b6411bbb74c9
Fredy Acuna <fredhiixd@gmail.com>
2020-02-20 21:52:57 +0000

    Added more pointer advanced tasks

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_number - Prints integer
+ * @n: Integer to print.
+ * Return: none.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if ((n / 10) > 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}

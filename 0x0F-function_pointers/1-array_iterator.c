3024ccab768a768113e5217dcf70b45ad75fe463
Fredy Acuna <fredhiixd@gmail.com>
2020-03-11 18:46:25 +0000

    Fixed: C - Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,8 +10,7 @@ void array_iterator(int *array, int size, void (*action)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
-	{
-		(*action)(array[i]);
-	}
+	if (array && size && action)
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,19 +16,19 @@ int main(int ac, char **av)
 		printf("Error\n");
 		exit(98);
 	}
-	if (av[2][0] != '+' && av[2][0] != '-' && av[2][0] != '*' &&
-	    av[2][0] != '/' && av[2][0] != '%' || av[2][1] != '\0')
+	if ((av[2][0] != '+' && av[2][0] != '-' && av[2][0] != '*' &&
+	    av[2][0] != '/' && av[2][0] != '%') || av[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	num1 = atoi(av[1]);
+	num2 = atoi(av[3]);
 	if (num2 == 0 && (av[2][0] == '/' || av[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	num1 = atoi(av[1]);
-	num2 = atoi(av[3]);
 	res = (*get_op_func(av[2]))(num1, num2);
 	printf("%i\n", res);
 

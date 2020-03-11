d191166ee485c445d1a118475b69c78f30508b26
Fredy Acuna <fredhiixd@gmail.com>
2020-03-11 18:59:17 +0000

    Fixed: C - Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,14 +8,13 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, element;
+	int i;
 
 	if (size <= 0)
 		return (-1);
-
 	if (array && size && cmp)
 		for (i = 0; i < size; i++)
-			if ((*cmp)(array[i]) == 1)
+			if ((*cmp)(array[i]) != 0)
 				return (i);
 
 	return (-1);

e477f2a5806ac292204bd3d537e5cb381b8e4a3d
fredhii <fredhiixd@gmail.com>
2020-03-24 08:42:57 -0500

    Fix #2: Added semicolon

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		current = current->next;
 		if (current == NULL)
-			return (NULL)
+			return (NULL);
 	}
 	new = malloc(sizeof(listint_t));
 	if (!new)

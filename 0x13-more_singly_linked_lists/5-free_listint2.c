76d6637ce88ec799f1e747f937eafb511e5a12db
fredhii <fredhiixd@gmail.com>
2020-03-24 08:08:08 -0500

    Fix #3: Changed next node assignation

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current;
 	listint_t *del;
 
+	current = *head;
 	while (current != NULL && head != NULL)
 	{
 		del = current;
-		current = del->next;
+		current = current->next;
 		free(del);
 	}
 	*head = NULL;

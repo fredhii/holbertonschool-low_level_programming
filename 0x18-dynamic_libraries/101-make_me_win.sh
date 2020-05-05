5cbfcafde7bcb93767e7841e915f815eeeabd047
fredhii <fredhiixd@gmail.com>
2020-05-04 19:30:45 -0500

    [Fix]: 0x18 advanced task #2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/fredhii/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/jackpoint.so
+wget -P /tmp https://github.com/fredhii/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/jackpoint.so
 export LD_PRELOAD=/tmp/jackpoint.so
